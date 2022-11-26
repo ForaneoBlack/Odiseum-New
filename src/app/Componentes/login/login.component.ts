d1cc39e00eee655a3a173cc221a2c397d498650f
Albertukis <alberto.vallejo.est@tecazuay.edu.ec>
2022-11-26 18:33:00 -0500

    login acabado

--- src/app/Componentes/login/login.component.ts
+++ src/app/Componentes/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, NgForm, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+
+
+import { LoginService } from 'src/app/Service/login.service';
+import { Userlogin } from 'src/app/Models/userlogin';
+import Swal from 'sweetalert2';
+import { rol_id } from '../../Models/userlogin';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,19 +14,59 @@ import {Router} from "@angular/router";
 })
 export class LoginComponent implements OnInit {
 
-  form: FormGroup;
+  issloading = true;
+  erro = "";
+  //Obtencion de datos de sesion
+  public userRequest: Userlogin = new Userlogin();
 
-  constructor(private fb: FormBuilder, private router: Router) {
-    this.form = this.fb.group(
-      ''
-     // username: ['', Validators.required],
-     // password: ['',Validators.required]
-    )
+  constructor(private router: Router, private LoginService: LoginService) 
+  {}
+
+  ngOnInit(): void {}
+
+  loginAuth(): void {
+    sessionStorage.clear;
+    this.LoginService.Login(this.userRequest).subscribe(
+      data => {
+        if(data!=null){
+          sessionStorage.clear;
+        sessionStorage.setItem('usuario', JSON.stringify(data));
+          
+          this.router
+            .navigate(["/dashboard/inicio"])
+            .then(() => {
+              
+            });
+          
+        }else{
+          Swal.fire({
+            title: "Error",
+            text: "Credenciales incorrectas",
+            icon: "warning",
+            color: "#FDFEFE",
+            confirmButtonColor: "#0c3255",
+            background: "#EBEE93 ",
+          });
+
+        }
+        
+        
+        
+      },
+      (err) => {
+        500;
+        console.log(err);
+        Swal.fire({
+          title: "Error",
+          text: "Credenciales incorrectas",
+          icon: "warning",
+          color: "#FDFEFE",
+          confirmButtonColor: "#0c3255",
+          background: "#EBEE93 ",
+        });
+      }
+    );
   }
-
-  ngOnInit(): void {
-  }
-
 
 
 }
